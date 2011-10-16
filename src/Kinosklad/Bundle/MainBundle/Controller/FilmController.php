847e5638d3e0da51519459d08f68f6dc03e94d26
everzet <ever.zet@gmail.com>
2011-10-16 10:53:50 +0300

    generated films controller
    app/console generate:doctrine:crud

---
+++ src/Kinosklad/Bundle/MainBundle/Controller/FilmController.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Kinosklad\Bundle\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Kinosklad\Bundle\MainBundle\Entity\Film;
+use Kinosklad\Bundle\MainBundle\Form\FilmType;
+
+/**
+ * Film controller.
+ *
+ */
+class FilmController extends Controller
+{
+    /**
+     * Lists all Film entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entities = $em->getRepository('KinoskladMainBundle:Film')->findAll();
+
+        return $this->render('KinoskladMainBundle:Film:index.html.twig', array(
+            'entities' => $entities
+        ));
+    }
+
+    /**
+     * Finds and displays a Film entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('KinoskladMainBundle:Film')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Film entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('KinoskladMainBundle:Film:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+
+        ));
+    }
+
+    /**
+     * Displays a form to create a new Film entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Film();
+        $form   = $this->createForm(new FilmType(), $entity);
+
+        return $this->render('KinoskladMainBundle:Film:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        ));
+    }
+
+    /**
+     * Creates a new Film entity.
+     *
+     */
+    public function createAction()
+    {
+        $entity  = new Film();
+        $request = $this->getRequest();
+        $form    = $this->createForm(new FilmType(), $entity);
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('films_show', array('id' => $entity->getId())));
+            
+        }
+
+        return $this->render('KinoskladMainBundle:Film:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Film entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('KinoskladMainBundle:Film')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Film entity.');
+        }
+
+        $editForm = $this->createForm(new FilmType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('KinoskladMainBundle:Film:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Film entity.
+     *
+     */
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('KinoskladMainBundle:Film')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Film entity.');
+        }
+
+        $editForm   = $this->createForm(new FilmType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        $request = $this->getRequest();
+
+        $editForm->bindRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('films_edit', array('id' => $id)));
+        }
+
+        return $this->render('KinoskladMainBundle:Film:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Film entity.
+     *
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = $em->getRepository('KinoskladMainBundle:Film')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Film entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('films'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
