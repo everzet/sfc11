2c1a12f0cf17ec7e9934ad53d00e0b44a6491f92
everzet <ever.zet@gmail.com>
2011-10-27 16:36:41 +0300

    removed non-used entity

--- src/Kinosklad/Bundle/MainBundle/Entity/User.php
+++ src/Kinosklad/Bundle/MainBundle/Entity/User.php
@@ -3,13 +3,11 @@
 namespace Kinosklad\Bundle\MainBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Entity\User as BaseUser;
 
 /**
  * Kinosklad\Bundle\MainBundle\Entity\User
  *
- * @ORM\Table()
  * @ORM\Entity
  */
 class User extends BaseUser
@@ -24,36 +22,6 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @var array $viewed
-     *
-     * @ORM\ManyToMany(targetEntity="Film")
-     * @ORM\JoinTable(name="user_viewed",
-     *     joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="film_id", referencedColumnName="id")}
-     * )
-     */
-    private $viewed;
-
-    /**
-     * @var array $favorites
-     *
-     * @ORM\ManyToMany(targetEntity="Film")
-     * @ORM\JoinTable(name="user_favorite",
-     *     joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="film_id", referencedColumnName="id")}
-     * )
-     */
-    private $favorites;
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->viewed    = new ArrayCollection();
-        $this->favorites = new ArrayCollection();
-    }
-
-    /**
      * Get id
      *
      * @return integer
@@ -61,45 +29,5 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * View film
-     *
-     * @param Film $film
-     */
-    public function addViewed(Film $film)
-    {
-        $this->getViewed()->add($film);
-    }
-
-    /**
-     * Get viewed films
-     *
-     * @return ArrayCollection
-     */
-    public function getViewed()
-    {
-        return $this->viewed;
-    }
-
-    /**
-     * Set favorite
-     *
-     * @param Film $favorite
-     */
-    public function addFavorite(Film $film)
-    {
-        $this->getFavorites()->add($film);
-    }
-
-    /**
-     * Get favorites
-     *
-     * @return ArrayCollection
-     */
-    public function getFavorites()
-    {
-        return $this->favorites;
     }
 }
